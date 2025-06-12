b70646a4266d0fc679a0c96e823dd08c5e5db07f
bhuvanesh-kp <bhuvaneshkpb@gmail.com>
2025-06-12 18:54:28 +0530

    connecting nats to project

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,18 +1,30 @@
-
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
-const stan = nats.connect('ticketing', 'abc',{
+const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
 
-stan.on('connect', () => {
-    const data = JSON.stringify({
+stan.on('connect', async () => {
+    const publisher = new TicketCreatedPublisher(stan);
+
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 47
+        });
+    }
+    catch (err) {
+        console.error(err);
+    }
+    /* const data = JSON.stringify({
         id: '1234',
         title: "myevent",
         price: 20
     });
 
     stan.publish('ticket:created', data , () => {
-
-    })
+        console.log(data);
+    }) */
 });
